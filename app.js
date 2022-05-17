447a8e9ea9e74f63a5de9c669047792621e65acb
amanzanares19 <amanzanares19@ilg.cat>
2022-05-17 19:46:30 +0200

    Ultim push

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 // El código invoca express() para crear una instancia de una aplicación de Express.
 const express = require('express');
 const app = express();
+
+function isAuthorized(req,res, next) {
+    const auth = req.headers.authorization;
+    if (auth === 'secretpassword') {
+      next();
+    } else {
+      res.status(401);
+      res.send('Not permitted');
+    }
+}
+
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-    app.get("/products", (req,res) => {
+
+// Primer ejemplo
+/* app.get("/products", (req,res) => {
     const products = [
     {
         id: 1,
@@ -23,6 +36,25 @@ app.get('/', (req, res) => res.send('Hello World!'));
     ];
 
     res.json(products);
-});
+});  */
+
+
+//Clients
+app.get('/users', isAuthorized, (req,res) => {
+    
+    res.json([{
+      
+      id: 1,
+      name: 'User Userson'
+    }])
+})
+  
+app.get('/products', (req, res) => {
+    res.json([{
+      id: 1,
+      name: 'The Bluest Eye'
+    }])
+})
+  
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
